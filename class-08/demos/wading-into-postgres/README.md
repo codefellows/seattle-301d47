89ee4bbee5dffc3b01d089c3b1aca75a3be86ea0
JB <jb.tellez@gmail.com>
2019-05-15 17:35:20 -0700

    adds class 8 demos

---
+++ class-08/demos/wading-into-postgres/README.md
@@ -0,0 +1,2 @@
+# lab-07-repository-301d47
+301d47 Lab 7 Repo
---
+++ class-08/demos/wading-into-postgres/server.js
@@ -0,0 +1,128 @@
+'use strict';
+
+// Application Dependencies
+const express = require('express');
+const superagent = require('superagent');
+const cors = require('cors');
+const pg = require('pg');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT || 3000;
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+
+app.get('/test/locations', (request, response) => {
+  
+  const dbQuery = 'SELECT * FROM locations;';
+
+  client.query(dbQuery)
+    .then(results => {
+      response.send(results.rows);
+  }).catch(error => handleError(error, response));
+
+
+});
+
+app.get('/test/add-location', (request, response) => {
+
+  const lat = request.query.latitude;
+  const lon = request.query.longitude;
+  const sq = 'whatever';
+  const fq = 'something';
+
+  const dbQuery = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ('${sq}', '${fq}',${lat},${lon});`;
+
+  client.query(dbQuery)
+    .then(results => {
+      response.send(results.rows);
+  }).catch(error => handleError(error, response));
+
+});
+
+// API Routes
+app.get('/location', (request, response) => {
+  searchToLatLong(request.query.data)
+    .then(location => response.send(location))
+    .catch(error => handleError(error, response));
+});
+
+app.get('/weather', getWeather);
+app.get('/events', getEvents);
+
+
+// Make sure the server is listening for requests
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// Error handler
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+// Models
+function Location(query, res) {
+  this.search_query = query;
+  this.formatted_query = res.body.results[0].formatted_address;
+  this.latitude = res.body.results[0].geometry.location.lat;
+  this.longitude = res.body.results[0].geometry.location.lng;
+}
+
+function Weather(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time * 1000).toString().slice(0, 15);
+}
+
+function Event(event) {
+  this.link = event.url;
+  this.name = event.name.text;
+  this.event_date = new Date(event.start.local).toString().slice(0, 15);
+  this.summary = event.summary;
+}
+
+function searchToLatLong(query) {
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API_KEY}`;
+
+  return superagent.get(url)
+    .then(res => {
+      return new Location(query, res);
+    })
+    .catch(error => handleError(error));
+}
+
+function getWeather(request, response) {
+  const url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
+
+  superagent.get(url)
+    .then(result => {
+      const weatherSummaries = result.body.daily.data.map(day => {
+        return new Weather(day);
+      });
+
+      response.send(weatherSummaries);
+    })
+    .catch(error => handleError(error, response));
+}
+
+
+function getEvents(request, response) {
+  const url = `https://www.eventbriteapi.com/v3/events/search?token=${process.env.EVENTBRITE_API_KEY}&location.address=${request.query.data.formatted_query}`;
+
+  superagent.get(url)
+    .then(result => {
+      const events = result.body.events.map(eventData => {
+        const event = new Event(eventData);
+        return event;
+      });
+
+      response.send(events);
+    })
+    .catch(error => handleError(error, response));
+}
