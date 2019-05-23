17fe12f489827e8e01d4319fb7f520c238c02487
JB <jb.tellez@gmail.com>
2019-05-22 19:24:22 -0700

    adds Book constructor with warning

--- class-13/skeleton/server.js
+++ class-13/skeleton/server.js
@@ -84,4 +84,16 @@ function deleteBook(request, response) {
 
 // Helper Functions
 
+// converts book info result from API to book instance
+// is NOT used to create Book instances when fetched from database
+function Book(info) {
+  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+
+  this.title = info.title ? info.title : 'No title available';
+  this.author = info.authors ? info.authors[0] : 'No author available';
+  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+  this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
+  this.description = info.description ? info.description : 'No description available';
+  this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
+}
 
